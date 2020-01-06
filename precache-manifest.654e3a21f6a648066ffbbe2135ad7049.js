774766b033cddcdd8a72168eb6a12544d5e23646
pmtavares <pedrotavares41@gmail.com>
2020-01-06 23:32:31 +0000

    Updates

---
+++ precache-manifest.654e3a21f6a648066ffbbe2135ad7049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bc3c3886cb6cea12fc1d5e16de5e6c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7ff97f41f8912a088846",
+    "url": "/monsters/static/css/main.5f621888.chunk.css"
+  },
+  {
+    "revision": "5e0cef974a56e28381bf",
+    "url": "/monsters/static/js/2.58453bf0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.58453bf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ff97f41f8912a088846",
+    "url": "/monsters/static/js/main.cdfe5207.chunk.js"
+  },
+  {
+    "revision": "fcc61893b7a9935ddc3e",
+    "url": "/monsters/static/js/runtime-main.9faa307c.js"
+  }
+]);
